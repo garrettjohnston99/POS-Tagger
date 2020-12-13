839644da4a74500d46b9fc9bfbfc96da042e9c07
Garrett Johnston <65191374+garrettjohnston99@users.noreply.github.com>
2020-12-13 11:10:10 -0500

    clean up style

--- POSTagger.java
+++ POSTagger.java
@@ -16,16 +16,27 @@ public class POSTagger {
     public static void main(String[] args) throws Exception {
         boolean test = false;
         boolean input = false;
-        List<Map<String, Map<String, Double>>> training = train(); // Training obs/transitions packed up
+        
+        // Get training maps (observations/transitions) packed into list
+        List<Map<String, Map<String, Double>>> training = train();
+        
+        // Test training data against user-selected testfiles
+        if (test) 
+            evaluateTagger(training); 
 
-        if (test) evaluateTagger(training); // Test training data against provided testfiles
-
-        if (input) while (true) inputTagger(training); // Tag words from user input
+        // take user input to test tagger
+        if (input) {
+            while (true) {
+                // Tag words from user input
+                inputTagger(training); 
+            }
+        }
+        
     }
 
 
     /**
-     * Given training weight of state transitions and observations, return the most likely sequence of states based
+     * Given training weight of state transitions and observations, return the most likely sequence of states(POS tags) based
      * on the training data
      * @param training List containing training data(observations, transitions)
      * @param line line of words to be tagged
@@ -34,10 +45,13 @@ public class POSTagger {
     public static String viterbi(List<Map<String, Map<String, Double>>> training, String line) {
         Map<String, Map<String, Double>> observations = training.get(0);
         Map<String, Map<String, Double>> transitions = training.get(1);
-        final double unseen = -100; // Observation penalty for a word not seen by a certain tag in training
+        final double unseen = -100;  // Penalty for a word not seen by a certain tag in training
 
-        String[] toTag = line.toLowerCase().split(" "); // Split line by spaces
-        if (toTag.length == 0) return ""; // Handle empty line
+        String[] toTag = line.toLowerCase().split(" ");
+        
+        // Handle empty line
+        if (toTag.length == 0) 
+            return "";  
 
         // List of maps of tag -> previous tag to traverse backwards later
         List<Map<String,String>> backTrace = new ArrayList<>(toTag.length - 1);
@@ -45,47 +59,50 @@ public class POSTagger {
         // Set and map of current tags, current tags -> score
         Set<String> currStates = new HashSet<>();
         Map<String, Double> currScores = new HashMap<>();
-        currStates.add("#"); currScores.put("#", 0.0); // Handle start case
+        currStates.add("#"); currScores.put("#", 0.0);  // Handle start case
 
-
-        // Interrogate each observation in line
+        // Interrogate each observation(word) in line
         for (int i = 0; i < toTag.length; i++) {
-            Map<String, String> bt = new HashMap<>(); // To put in backTrace
-            Set<String> nextStates = new HashSet<>(); // States to visit on next iteration
-            Map<String, Double> nextScores = new HashMap<>();
+            Map<String, String> bt = new HashMap<>();  // To put in backTrace
+            Set<String> nextStates = new HashSet<>();  // States to visit on next iteration
+            Map<String, Double> nextScores = new HashMap<>();  // Scores for next iteration
+            
             for (String curr : currStates) {
                 // Consider all states reachable from this one
                 for (String next : transitions.get(curr).keySet()) {
                     nextStates.add(next);
                     // If next state observed in training, get score; otherwise, use unseen
                     double obScore = observations.get(next).getOrDefault(toTag[i], unseen);
+                    
                     // Calculate score for transition to this state: current + transition score + observation score
                     double nextScore = currScores.get(curr) + transitions.get(curr).get(next) + obScore;
-                    if (!(nextScores.containsKey(next))||(nextScore > nextScores.get(next))) { // New tag or better score
+                    
+                    // If there's a new tag or better score, put it in the map
+                    if (!(nextScores.containsKey(next)) || (nextScore > nextScores.get(next))) { 
                         nextScores.put(next, nextScore);
-                        bt.put(next, curr); // curr is next's predecessor
+                        bt.put(next, curr);  // curr is next's predecessor
                     }
                 }
             }
+            
+            // update current states and scores, add best score to 
             currStates = nextStates;
             currScores = nextScores;
             backTrace.add(bt);
         }
 
-
         // Get tag with max score to traverse maps in backTrace
         String tracer = null;
         for (String tag : currScores.keySet()) {
-            if (tracer == null || currScores.get(tag) > currScores.get(tracer)) {
+            if (tracer == null || currScores.get(tag) > currScores.get(tracer)) 
                 tracer = tag;
-            }
         }
 
-        Stack<String> tagStack = new Stack<>();
         // Add all tags to a stack to get them out in the right order
+        Stack<String> tagStack = new Stack<>();
         for (int i = backTrace.size() - 1; i >= 0; i--) {
             tagStack.push(tracer);
-            tracer = backTrace.get(i).get(tracer); // Traverse backwards
+            tracer = backTrace.get(i).get(tracer);  // Traverse backwards through backTrace
         }
 
         StringBuilder allTags = new StringBuilder();
@@ -102,21 +119,20 @@ public class POSTagger {
     /**
      * Trains HMM on user-selected text & tag files
      * @return List of observations(index 0), transitions(index 1) with log-probabilities
-     * observations: state -> {observation -> weight}
-     * transitions: state -> {state -> weight}
+     * observations: state -> {observation -> weight} / tag -> {word -> weight}
+     * transitions: state -> {state -> weight} / tag -> {tag -> weight} 
      */
     public static List<Map<String, Map<String, Double>>> train() throws IOException {
         // Select training files
-        String obs = getPath(0); // Training sentences file
-        String tags = getPath(1); // Training tags file
+        String obs = getPath(0);  // Training sentences file
+        String tags = getPath(1);  // Training tags file
         Scanner obsScanner = new Scanner(new FileInputStream(obs));
         Scanner tagsScanner = new Scanner(new FileInputStream(tags));
 
         // Observation/transition maps to place frequencies in
-        Map<String, Map<String, Double>> observations = new HashMap<>(); // tag -> (word -> freq)
-        Map<String, Map<String, Double>> transitions = new HashMap<>(); // tag -> (other tag -> freq)
-        transitions.put("#", new HashMap<>()); // Start character
-
+        Map<String, Map<String, Double>> observations = new HashMap<>();  // tag -> (word -> freq)
+        Map<String, Map<String, Double>> transitions = new HashMap<>();  // tag -> (other tag -> freq)
+        transitions.put("#", new HashMap<>());  // Start character
 
         try {
             while (obsScanner.hasNextLine()) {
@@ -125,38 +141,54 @@ public class POSTagger {
                 String[] t = tagsScanner.nextLine().toLowerCase().split(" "); // Respective tags
 
                 for (int i = 0; i < o.length; i++) {
-                    // Observations: current tag -> current word, freq
-                    if (!observations.containsKey(t[i])) { // Haven't seen this tag before
-                        observations.put(t[i], new HashMap<>());
-                        observations.get(t[i]).put(o[i], 1.0); // Place tag w/ word
-                    } else if (!observations.get(t[i]).containsKey(o[i])) { // Seen current tag, but not current word
-                        observations.get(t[i]).put(o[i], 1.0); // Place current word in current tag's map
+                    String word = o[i];
+                    String tag = t[i];
+                    
+                    /*
+                     * Observations: current tag -> current word, freq
+                     */ 
+                    
+                    if (!observations.containsKey(tag) { // Haven't seen this tag before
+                        observations.put(tag, new HashMap<>());
+                        observations.get(tag).put(word, 1.0); // Place tag w/ word
+                    } else if (!observations.get(tag).containsKey(word)) { // Seen current tag, but not current word
+                        observations.get(tag).put(word, 1.0); // Place current word in current tag's map
                     } else { // Seen this tag, and this word. Increment frequency
-                        observations.get(t[i]).put(o[i], observations.get(t[i]).get(o[i]) + 1);
+                        observations.get(tag).put(word, observations.get(tag).get(word) + 1);
                     }
 
-                    // Transitions: current tag -> next tag, frequency
-                    if (i >= o.length - 1) { // Last word in line; no transition to i+1
-                        if (!transitions.containsKey(t[i])) transitions.put(t[i], new HashMap<>());
+                    /* 
+                     * Transitions: current tag -> next tag, frequency
+                     */
+                    
+                    if (i == 0) { 
+                        // Deal with # -> first word
+                        if (!transitions.get("#").containsKey(tag)) { 
+                            // Haven't seen this tag as first in a line
+                            transitions.get("#").put(tag, 1.0);
+                        } else { 
+                            // Seen tag as first in a line, increment frequency
+                            transitions.get("#").put(tag, transitions.get("#").get(tag) + 1.0);
+                        }
+                    } else if (i == o.length - 1) { 
+                        // Last word in line; no transition to i+1
+                        if (!transitions.containsKey(tag)) 
+                            transitions.put(tag, new HashMap<>());
                         continue;
                     }
-
-                    if (i == 0) { // Deal with # -> first word
-                        if (!transitions.get("#").containsKey(t[i])) { // Haven't seen this tag a first in a line
-                            transitions.get("#").put(t[i], 1.0);
-                        } else { // Seen tag as first in a line, increment frequency
-                            transitions.get("#").put(t[i], transitions.get("#").get(t[i]) + 1.0);
-                        }
-                    }
-
-                    if (!transitions.containsKey(t[i])) { // Haven't seen this tag before
-                        transitions.put(t[i], new HashMap<>());
-                        transitions.get(t[i]).put(t[i + 1], 1.0);
+                    
+                    // Consider tag -> nextTag transition
+                    String nextTag = t[i + 1];
+                    if (!transitions.containsKey(tag)) { 
+                        // Haven't seen this tag before
+                        transitions.put(tag, new HashMap<>());
+                        transitions.get(tag).put(nextTag, 1.0);
+                    } else if (!transitions.get(tag).containsKey(nextTag)) {
                         // Seen this tag, but haven't seen next tag directly after this one
-                    } else if (!transitions.get(t[i]).containsKey(t[i + 1])) {
-                        transitions.get(t[i]).put(t[i + 1], 1.0);
-                    } else { // Seen t[i] and t[i+1] consecutively. Increment frequency
-                        transitions.get(t[i]).put(t[i + 1], transitions.get(t[i]).get(t[i + 1]) + 1);
+                        transitions.get(tag).put(nextTag, 1.0);
+                    } else { 
+                        // Seen tag and nextTag consecutively. Increment frequency of that sequence
+                        transitions.get(tag).put(nextTag, transitions.get(tag).get(nextTag) + 1.0);
                     }
             }
         }
@@ -165,11 +197,11 @@ public class POSTagger {
             tagsScanner.close();
         }
 
-        // Convert to log-probability
+        // Convert to log-probability in place
         freqToWeight(observations);
         freqToWeight(transitions);
 
-        // Pack up
+        // Pack up into list
         List<Map<String, Map<String, Double>>> weights = new ArrayList<>();
         weights.add(observations);
         weights.add(transitions);
@@ -184,7 +216,7 @@ public class POSTagger {
      */
     public static void freqToWeight(Map<String, Map<String, Double>> freq) {
         for (String outer : freq.keySet()) {
-            double tot = 0; // Sum total occurrences
+            double tot = 0;  // Sum total occurrences
             for (String inner : freq.get(outer).keySet()) {
                 tot += freq.get(outer).get(inner);
             }
@@ -213,10 +245,10 @@ public class POSTagger {
      * @param training observations & transitions packed in list
      */
     public static void evaluateTagger(List<Map<String, Map<String, Double>>> training) throws IOException {
-        int correct = 0; // Number of correctly tagged words
+        int correct = 0;  // Number of correctly tagged words
         int total = 0;
-        String testObs = getPath(2); // Test sentences file
-        String testTags = getPath(3); // Test tags file
+        String testObs = getPath(2);  // Test sentences file
+        String testTags = getPath(3);  // Test tags file
         Scanner testObsScanner = new Scanner(new FileInputStream(testObs));
         Scanner testTagsScanner = new Scanner(new FileInputStream(testTags));
 
